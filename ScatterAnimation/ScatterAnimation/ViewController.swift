4a07694cabbda3fde1851e923db635496b08885c
Donovan Langager <donovan.lan16@gmail.com>
2019-09-10 15:09:56 -0700

    Added labels and lambda logo

---
+++ ScatterAnimation/ScatterAnimation/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ScatterAnimation
+//
+//  Created by Uptiie on 9/10/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lLabel: UILabel!
+    @IBOutlet weak var aLabel: UILabel!
+    @IBOutlet weak var mLabel: UILabel!
+    @IBOutlet weak var bLabel: UILabel!
+    @IBOutlet weak var dLabel: UILabel!
+    @IBOutlet weak var a2Label: UILabel!
+    @IBOutlet weak var lambdaLogoImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func toggleButtonPressed(_ sender: Any) {
+        
+        var isScattered: Bool
+    }
+    
+}
+
